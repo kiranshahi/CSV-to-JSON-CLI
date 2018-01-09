673dd359de9a6e8fcdc58acfb1daae05d4f6b817
Kiran Shahi <kiran.shahi.np@gmail.com>
2018-01-09 22:36:33 +0545

    parse to json done.

--- csv2json.py
+++ csv2json.py
@@ -1,7 +1,12 @@
 import csv
+import json
+import sys
 
 with open('test.csv', 'r') as csv_file:
-    csv_reader = csv.reader(csv_file)
-    
-    for line in csv_reader:
-        print(line)
+    csv_content = csv.DictReader(csv_file)   
+    with open('new.json', 'w') as new_file:
+        new_file.write('[')
+        for row in csv_content:
+            json.dump(row, new_file)
+                new_file.write(',\n')
+        new_file.write(']')
