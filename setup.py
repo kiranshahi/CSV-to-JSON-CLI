b2e66e19694f2a0c25035ab8e2f6c3b5ddd2856a
Kiran Shahi <kiran.shahi.np@gmail.com>
2018-01-10 09:21:28 +0545

    setup.py updated.

--- setup.py
+++ setup.py
@@ -3,5 +3,10 @@ from setuptools import setuptools
 setup(
     name='CSV-to-JSON',
     version='0.1',
+    license='GPL-3.0',
+    description='A command line tool to convert csv file into json file',
+    author='Kiran Shahi',
+    auther_email='kiran.shahi.np@gmail.com',
+    url='https://github.com/kiranshahi/CSV-to-JSON-CLI',
     scripts=['csv2json']
 )
