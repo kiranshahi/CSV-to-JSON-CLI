16e27e5d595331b354ea178312507ae183d17f24
Kiran Shahi <kiran.shahi.np@gmail.com>
2018-01-10 09:27:00 +0545

    file generated based on parameter.

--- csv2json.py
+++ csv2json.py
@@ -2,9 +2,9 @@ import csv
 import json
 import sys
 
-with open('test.csv', 'r') as csv_file:
+with open(sys.argv[1], 'r') as csv_file:
     csv_content = csv.DictReader(csv_file)   
-    with open('new.json', 'w') as new_file:
+    with open(sys.argv[2], 'w') as new_file:
         new_file.write('[\n')
         for row in csv_content:
             json.dump(row, new_file)
