2c27afb7e56c497c1b38c76de2b5c49d6856da32
Kiran Shahi <kiran.shahi.np@gmail.com>
2018-01-10 09:05:22 +0545

    csv2json.py has been updated

--- csv2json.py
+++ csv2json.py
@@ -5,8 +5,8 @@ import sys
 with open('test.csv', 'r') as csv_file:
     csv_content = csv.DictReader(csv_file)   
     with open('new.json', 'w') as new_file:
-        new_file.write('[')
+        new_file.write('[\n')
         for row in csv_content:
             json.dump(row, new_file)
-                new_file.write(',\n')
+            new_file.write(',\n')
         new_file.write(']')
