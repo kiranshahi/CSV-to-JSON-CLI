11113445eaa10fbb9cad822159bb3de597272bba
Kiran Shahi <kiran.shahi.np@gmail.com>
2019-01-17 11:13:43 +0545

    script updated.

--- csv2json.py
+++ csv2json.py
@@ -7,6 +7,6 @@ with open(sys.argv[1], encoding='utf-8', mode='r') as csv_file:
     with open(sys.argv[2], encoding='utf-8', mode='w') as new_file:
         new_file.write('[\n')
         for row in csv_content:
-            json.dump(row, new_file, ensure_ascii=False)
-            new_file.write(',\n')
+        	json.dump(row, new_file, ensure_ascii=False)
+        	new_file.write(',\n')
         new_file.write(']')
