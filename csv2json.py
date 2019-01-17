83df4136442c741348cde161eb6e75cda248721a
Kiran Shahi <kiran.shahi.np@gmail.com>
2019-01-17 10:50:10 +0545

    Script updated to support unicode character.

--- csv2json.py
+++ csv2json.py
@@ -2,11 +2,11 @@ import csv
 import json
 import sys
 
-with open(sys.argv[1], 'r') as csv_file:
+with open(sys.argv[1], encoding='utf-8', mode='r') as csv_file:
     csv_content = csv.DictReader(csv_file)   
-    with open(sys.argv[2], 'w') as new_file:
+    with open(sys.argv[2], encoding='utf-8', mode='w') as new_file:
         new_file.write('[\n')
         for row in csv_content:
-            json.dump(row, new_file)
+            json.dump(row, new_file, ensure_ascii=False)
             new_file.write(',\n')
         new_file.write(']')
